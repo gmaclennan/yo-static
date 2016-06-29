778756b5b3a3fbf86a4c375c55c5ae5e21f36669
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-06-28 17:58:46 -0700

    Copy all static assets into site folder on yo-static build

--- scripts/build-site.js
+++ scripts/build-site.js
@@ -5,11 +5,13 @@ module.exports = function buildSite (argv) {
   argv = argv || {}
   buildContent(argv, function (err) {
     if (err) return console.error('Error building content:', err)
-    var pending = 2
+    var pending = 3
 
     var buildPages = require('./build-pages')
     var buildJavascript = require('./build-javascript')
+    var buildStatic = require('../scripts/build-static')
 
+    buildStatic(argv, done)
     buildPages(argv, done)
     buildJavascript(done)
 
---
+++ scripts/build-static.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const fs = require('fs-extra')
+const path = require('path')
+const glob = require('glob')
+const mkdirp = require('mkdirp')
+const parallelLimit = require('run-parallel-limit')
+
+var cwd = process.cwd()
+
+var gitignore = fs.readFileSync(path.join(cwd, '.gitignore'), 'utf8')
+  .split('\n')
+  .filter(s => s.length && s.slice(0, 1) !== '#')
+
+module.exports = buildStatic
+
+function buildStatic (config, cb) {
+  config = config || {}
+  Object.assign(config, require('../lib/config'))
+
+  mkdirp.sync(config.site_dir)
+
+  console.time('build static assets')
+
+  glob('!(_|.){*,**/*}', {
+    cwd: process.cwd(),
+    ignore: config.blacklist.concat(gitignore),
+    nodir: true
+  }, function (err, files) {
+    if (err) return cb ? cb(err) : console.error(err)
+    if (!files.length) return cb && cb()
+
+    const tasks = files.map(function (file) {
+      const src = path.resolve(cwd, file)
+      const dst = path.join(config.site_dir, file)
+      return function (cb) {
+        fs.copy(src, dst, {clobber: true}, cb)
+      }
+    })
+
+    parallelLimit(tasks, 10, function (err) {
+      console.timeEnd('build static assets')
+      cb ? cb(err) : err && console.error(err)
+    })
+  })
+}
